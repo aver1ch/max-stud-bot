edf463ddfd2a90705571e52a3a431f5b56201cc8
encore180 <astegosgrs@gmail.com>
2025-11-06 22:46:51 +0200

    base

---
+++ frontend/src/App.jsx
@@ -0,0 +1,45 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <div>
+      <header className="header">
+        <div className="container">
+          <img src="/polylogo.svg" alt="PolyLogo" className="logo" />
+          {/* <img src="/notify.svg" alt="Notification" className="noty"/> */}
+        </div>
+      </header>
+      <main className="main">
+        <div className="container">
+          <div className="card">
+            <h1 className="card-title">Информация об обучающемся</h1>
+            <div className="card-student-info">
+              <div className="student-top">
+                <img src="/studphoto.png" alt="Student" className="studImg" />
+                <div className="student-name">
+                  <h2>Аверич Владимир Евгеньевич</h2>
+                </div>
+              </div>
+              <div className="student-main-info">
+                <p>Институт компьютерных наук и технологий</p>
+                <p>Группа: 5132701/20101</p>
+                <p>Общежитие: №1</p>
+                <p>Номер телефона: +7XXXXXXXXX</p>
+                <p>Email: jopa@edu.spbstu.ru</p>
+                <p>Выговоры: 0</p>
+                <p>LeaderID: 0</p>
+              </div>
+            </div>
+            <nav className="nav">
+              <button className="btn">Студенческий клуб</button>
+              <button className="btn">Общежитие</button>
+              <button className="btn">Расписание</button>
+            </nav>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;

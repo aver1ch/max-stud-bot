e3c731fff0975443782527a4de7f8aa321756ea1
encore180 <astegosgrs@gmail.com>
2025-11-06 23:15:57 +0200

    back button

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,24 +1,21 @@
 import React from "react";
 import "./Login.css";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
   return (
     <div className="login-wrap">
       <h1 className="login-title">Вход</h1>
 
-      <input
-        type="text"
-        placeholder="Логин"
-        className="login-input"
-      />
+      <input type="text" placeholder="Логин" className="login-input" />
 
-      <input
-        type="password"
-        placeholder="Пароль"
-        className="login-input"
-      />
+      <input type="password" placeholder="Пароль" className="login-input" />
 
       <button className="login-btn">Войти</button>
+      <button className="login-btn" onClick={() => navigate("/")}>
+        Назад
+      </button>
     </div>
   );
 }

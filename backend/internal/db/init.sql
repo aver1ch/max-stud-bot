07f408e8190bff4f7d8c86999c717c8190b48e1b
aver1ch <vavericherry3@gmail.com>
2025-11-14 13:00:49 +0300

    added nginx

--- backend/internal/db/init.sql
+++ backend/internal/db/init.sql
@@ -2,6 +2,8 @@ CREATE TABLE IF NOT EXISTS students (
     id SERIAL PRIMARY KEY,
     login TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL
+    numberOfGroup TEXT NOT NULL
+    numberOfGradeBook INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS employers (
@@ -13,7 +15,7 @@ CREATE TABLE IF NOT EXISTS employers (
 
 
 INSERT INTO students (login, password)
-VALUES ('averich.ve@edu.spbstu.ru', '123'),
-       ('averich.pa@edu.spbstu.ru', 'hahich321'),
-       ('kringalov.sv@edu.spbstu.ru', 'ktonacpal2003')
+VALUES ('averich.ve@edu.spbstu.ru', '123', '5132701/20001'),
+       ('averich.pa@edu.spbstu.ru', 'hahich321', '5132701/20002'),
+       ('kringalov.sv@edu.spbstu.ru', 'ktonacpal2003', '5132701/20003'),
 ON CONFLICT DO NOTHING;

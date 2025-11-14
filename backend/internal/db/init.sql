b881621247d461a4b173572e8f480c11ba48d847
aver1ch <vavericherry3@gmail.com>
2025-11-14 14:08:08 +0300

    added fixes

--- backend/internal/db/init.sql
+++ backend/internal/db/init.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS students (
     id SERIAL PRIMARY KEY,
     login TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
-    numberOfGroup TEXT NOT NULL
+    password TEXT NOT NULL,
+    numberOfGroup TEXT NOT NULL,
     numberOfGradeBook INTEGER NOT NULL
 );
 
@@ -12,6 +12,18 @@ CREATE TABLE IF NOT EXISTS employers (
     password TEXT NOT NULL,
     codePassword TEXT NOT NULL
 );
+
+CREATE TABLE grades (
+    id SERIAL PRIMARY KEY,
+    student_id INT REFERENCES students(id) ON DELETE CASCADE,
+    subject_id INT REFERENCES subjects(id) ON DELETE CASCADE,
+    semester INT NOT NULL,
+    teacher TEXT NOT NULL,
+    control_type TEXT NOT NULL,
+    grade INT NOT NULL,
+    UNIQUE(student_id, subject_id, semester)
+);
+
 
 
 INSERT INTO students (login, password)
